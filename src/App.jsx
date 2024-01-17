141a3bf39e278cd77d628b5b64f398f8f92f5181
abdullah5361k <abdullah5361k@gmail.com>
2024-01-17 23:41:35 +0500

    added signup flow

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import HomePage from "./Pages/HomePage";
 import AboutUsPage from "./Pages/AboutUs"
 import NotFound from './Pages/NotFound';
+import Signup from './Pages/SignUp';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/about' element={<AboutUsPage />}></Route>
+        <Route path='/signup' element={<Signup />} ></Route>
         <Route path='*' element={<NotFound />} ></Route>
       </Routes>
     </>
